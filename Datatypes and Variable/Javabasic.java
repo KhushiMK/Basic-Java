cfb7e7f6283149abb1e42e27c89e40bfa0a506b7
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2023-04-02 10:15:12 +0530

    Add files via upload

---
+++ Datatypes and Variable/Javabasic.java
@@ -0,0 +1,16 @@
+
+public class Javabasic
+{
+    public static void main(String args[])
+    {
+        short a = 10;
+        byte b = 15;
+        char c = 'a';
+        int i = 10;
+        float k = 12.4f;
+        long t = 100;
+        double d = 100.5;
+        double ans = a + b + c + i + k + t + d;
+        System.out.println(ans);
+    }
+}
